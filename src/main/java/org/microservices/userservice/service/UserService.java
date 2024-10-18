88563007d5cd0222ceddffe590ce5e9ababfd2ff
amahdavid <amahdavid34@gmail.com>
2024-10-17 21:38:06 -0700

    Created Order and User service classes

---
+++ src/main/java/org/microservices/userservice/service/UserService.java
@@ -0,0 +1,25 @@
+package org.microservices.userservice.service;
+
+import org.microservices.userservice.model.User;
+import org.microservices.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
