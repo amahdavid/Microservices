8f93431636ae16bc9ab7a51c665d5f79117cfdae
amahdavid <amahdavid34@gmail.com>
2024-10-17 23:09:44 -0700

    Added an AppConfig class, update spring boot version

--- src/main/java/org/microservices/orderservice/service/OrderService.java
+++ src/main/java/org/microservices/orderservice/service/OrderService.java
@@ -2,8 +2,10 @@ package org.microservices.orderservice.service;
 
 import org.microservices.orderservice.model.Order;
 import org.microservices.orderservice.repository.OrderRepository;
+import org.microservices.userservice.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -11,6 +13,9 @@ import java.util.List;
 public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public List<Order> getAllOrders() {
         return orderRepository.findAll();
@@ -21,6 +26,8 @@ public class OrderService {
     }
 
     public Order createOrder(Order order) {
+        String USER_SERVICE_URL = "http://localhost:8081/users";
+        User user = restTemplate.getForObject(USER_SERVICE_URL + "/" + order.getUserId(), User.class);
         return orderRepository.save(order);
     }
 }
